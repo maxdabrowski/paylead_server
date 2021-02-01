30cd03b8ee7ccde9f1f0a0c1ccd8206be0f9709f
maks <maxdabrowski93@o2.pl>
2021-02-01 22:48:44 +0100

    dodanie Procfile

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { createServer } from 'http';
 import { router } from './rest-paylead';
 
 const app = express();
+var PORT = process.env.PORT || 9090;
 app.use('/api', router);
 app.use('/data', express.static(path.join(__dirname, '..', 'data')));
 const server = createServer(app);
-server.listen(9090, "localhost", () => {
+
+server.listen(PORT, () => {
   console.log('Listening on localhost:9090');
 });
